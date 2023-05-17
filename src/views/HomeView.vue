7b0093103feb23ca299fad4fb83990dbf9087c42
MuhammadMahbub <muhammadmahbub07@gmail.com>
2023-05-18 05:48:16 +0600

    install & setup

---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+
+
+<template>
+  <main class="container">
+    <h1>Home Page</h1>
+  </main>
+</template>
---
+++ src/views/StudentView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container">
+    <h1>This is student page</h1>
+  </div>
+</template>
+
+
